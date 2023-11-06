83ee90cc8662261060f8fcfd163ec09ece8d4047
Tam Tran <trancotam@gmail.com>
2023-11-06 20:11:55 +0700

    dom event
    Signed-off-by: Tam Tran <trancotam@gmail.com>

--- base-react-redux/src/components/MyComponents.js
+++ base-react-redux/src/components/MyComponents.js
@@ -5,10 +5,18 @@ class MyComponents extends React.Component {
         address: "HCM",
         age: "24"
     }
+    handleClick(event) {
+        console.log(">>>Click me", event.pageX)
+    }
+    handleOnMouseOver(event) {
+        console.log(">>>Click me", event)
+    }
     render() {
         return (
             <div>
                 My name is {this.state.name} and I am from {this.state.address}
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleOnMouseOver}>Click me</button>
             </div>
         );
     }
