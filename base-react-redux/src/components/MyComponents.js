fad4c6b954779af1d8b4a295aefdb94e6edf7a35
Tam Tran <trancotam@gmail.com>
2023-11-07 09:03:24 +0700

    form
    Signed-off-by: Tam Tran <trancotam@gmail.com>

--- base-react-redux/src/components/MyComponents.js
+++ base-react-redux/src/components/MyComponents.js
@@ -17,12 +17,30 @@ class MyComponents extends React.Component {
         console.log(">>>Click me", event)
         console.log("this.state.address", this.state.address)
     }
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value,
+            age: Math.floor(Math.random() * 100 + 1)
+        })
+        console.log(">>>handleOnChange ", event.target.value)
+    }
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        // alert("me")
+        console.log(">>>Submit ", this.state)
+    }
     render() {
         return (
             <div>
                 My name is {this.state.name} and I am from {this.state.address} , i am {this.state.age}
                 <button onClick={this.handleClick}>Click me</button>
                 <button onMouseOver={(event) => { this.handleOnMouseOver(event) }}>Click me</button>
+                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+                    <input
+                        type="text"
+                        onChange={(event => { this.handleOnChange(event) })} />
+                    <button>Submit</button>
+                </form>
             </div>
         );
     }
