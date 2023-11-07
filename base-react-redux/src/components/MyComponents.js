e12782945de20da7c8dc1566fc26e9a66d28dbc8
Tam Tran <trancotam@gmail.com>
2023-11-07 08:31:59 +0700

    set state
    Signed-off-by: Tam Tran <trancotam@gmail.com>

--- base-react-redux/src/components/MyComponents.js
+++ base-react-redux/src/components/MyComponents.js
@@ -5,18 +5,24 @@ class MyComponents extends React.Component {
         address: "HCM",
         age: "24"
     }
-    handleClick(event) {
+    handleClick = (event) => {
         console.log(">>>Click me", event.pageX)
+        console.log("this.state.address", this.state.address)
+        this.setState({
+            name: 'co tam',
+            age: Math.floor(Math.random() * 100 + 1)
+        })
     }
-    handleOnMouseOver(event) {
+    handleOnMouseOver = (event) => {
         console.log(">>>Click me", event)
+        console.log("this.state.address", this.state.address)
     }
     render() {
         return (
             <div>
-                My name is {this.state.name} and I am from {this.state.address}
+                My name is {this.state.name} and I am from {this.state.address} , i am {this.state.age}
                 <button onClick={this.handleClick}>Click me</button>
-                <button onMouseOver={this.handleOnMouseOver}>Click me</button>
+                <button onMouseOver={(event) => { this.handleOnMouseOver(event) }}>Click me</button>
             </div>
         );
     }
