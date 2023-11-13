6071b624d91ea8f12fef4059af066f6842e9f852
Tam Tran <trancotam@gmail.com>
2023-11-13 08:20:18 +0700

    fragment
    Signed-off-by: Tam Tran <trancotam@gmail.com>

--- base-react-redux/src/components/DisplayInfo.js
+++ base-react-redux/src/components/DisplayInfo.js
@@ -16,49 +16,54 @@ class DisplayInfo extends React.Component {
         console.log("props", this.props)
 
         return (
-            <div className="display-info-container">
-                <img src={logo} />
-                <div>
-                    <span onClick={() => { this.handleShowHide() }}>
-                        {this.state.isShowListUser ? "Hide listUser" : "Show listUser"}
-                    </span>
-                </div>
-                {this.state.isShowListUser &&
+            <>
+                <div className="display-info-container">
+                    <img src={logo} />
                     <div>
-                        {
-                            listUsers.map((user) => {
-                                console.log(">>user", user)
-                                return (
-                                    <div key={user.id} className={(+user.age > 18) ? "red" : "green"}>
-                                        <div> My name is {user.name}</div>
-                                        {/* <div style={{ color: "yellow", paddingTop: "50px" }}> My name is {user.name}</div> */}
-                                        <div> I am {user.age}</div>
-                                        <hr />
-                                    </div>
-                                )
-                                // if (+user.age > 18) {
-                                //     return (
-                                //         <div key={user.id} className="red">
-                                //             <div> My name is {user.name}</div>
-                                //             <div> I am {user.age}</div>
-                                //             <hr />
-                                //         </div>
-                                //     )
-                                // } else {
-                                //     return (
-                                //         <div key={user.id} className="green">
-                                //             <div> My name is {user.name}</div>
-                                //             <div> I am {user.age}</div>
-                                //             <hr />
-                                //         </div>
-                                //     )
-                                // }
-
-                            })
-                        }
+                        <span onClick={() => { this.handleShowHide() }}>
+                            {this.state.isShowListUser ? "Hide listUser" : "Show listUser"}
+                        </span>
                     </div>
-                }
-            </div>
+                    {this.state.isShowListUser &&
+                        <div>
+                            {
+                                listUsers.map((user) => {
+                                    console.log(">>user", user)
+                                    return (
+                                        <div key={user.id} className={(+user.age > 18) ? "red" : "green"}>
+                                            <div> My name is {user.name}</div>
+                                            {/* <div style={{ color: "yellow", paddingTop: "50px" }}> My name is {user.name}</div> */}
+                                            <div> I am {user.age}</div>
+                                            <hr />
+                                        </div>
+                                    )
+                                    // if (+user.age > 18) {
+                                    //     return (
+                                    //         <div key={user.id} className="red">
+                                    //             <div> My name is {user.name}</div>
+                                    //             <div> I am {user.age}</div>
+                                    //             <hr />
+                                    //         </div>
+                                    //     )
+                                    // } else {
+                                    //     return (
+                                    //         <div key={user.id} className="green">
+                                    //             <div> My name is {user.name}</div>
+                                    //             <div> I am {user.age}</div>
+                                    //             <hr />
+                                    //         </div>
+                                    //     )
+                                    // }
+
+                                })
+                            }
+                        </div>
+                    }
+                </div>
+                <div className="test">
+                    Tam
+                </div>
+            </>
             // <div>
             //     My Nam is {this.props.name}
             //     I am {this.props.age}
