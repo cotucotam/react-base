5e2fc26b14dc97e6c931a3a6ba431cedad3dc820
Tam Tran <trancotam@gmail.com>
2023-11-20 08:36:11 +0700

    homepage design
    Signed-off-by: Tam Tran <trancotam@gmail.com>

--- base-react-redux/src/components/Home/Homepage.js
+++ base-react-redux/src/components/Home/Homepage.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import videoHomePage from "../../assets/homepage.mp4"
 const Homepage = () => {
     return (
         <div className='homepage-container'>
-            Home Page
+            <video autoPlay muted loop width="750" height="500">
+                <source
+                    src={videoHomePage}
+                    type="video/mp4" />
+            </video>
         </div>
     );
 };
