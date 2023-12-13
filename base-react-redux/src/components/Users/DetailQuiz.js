5db7735cf3a403ffc8d21b1b89220a0b97afffea
Tam Tran <trancotam@gmail.com>
2023-12-13 09:14:04 +0700

    xu ly data in frontend
    Signed-off-by: Tam Tran <trancotam@gmail.com>

--- base-react-redux/src/components/Users/DetailQuiz.js
+++ base-react-redux/src/components/Users/DetailQuiz.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDataQuizId } from '../../services/apiService';
+import _ from 'lodash'
 const DetailQuiz = () => {
     const params = useParams()
     const quizId = params.id
@@ -8,7 +9,33 @@ const DetailQuiz = () => {
     useEffect(() => { fetchQuestion() }, [quizId])
     const fetchQuestion = async () => {
         let res = await getDataQuizId(quizId)
+        let questionDescription, image = null
         console.log("question, ", res)
+        if (res && res.EC === 0) {
+            let raw = res.DT
+            let data =
+                _.chain(raw)
+                    // Group the elements of Array based on `color` property
+                    .groupBy("id")
+                    // `key` is group's name (color), `value` is the array of objects
+                    .map((value, key) => {
+                        let answers = []
+                        value.forEach((item, index) => {
+                            if (index === 0) {
+                                questionDescription = item.questionDescription
+                                image = item.image
+                            }
+                            answers.push(item)
+                        })
+
+
+
+                        return { questionId: key, answers, questionDescription, image }
+                    }
+                    )
+                    .value()
+            console.log("data", data)
+        }
     }
     return (
         <div>
